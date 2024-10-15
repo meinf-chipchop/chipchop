1bbbd1359b5362f8ea36477199b45e9d6730ad1d
Christian López García <drwn.it@gmail.com>
2024-10-15 19:05:06 +0200

    Orders first model

--- api/orders/models.py
+++ api/orders/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
-# Create your models here.
+from users.models import CCDelivery
+
+
+User = get_user_model()
+
+class Order(models.Model):
+    
+    
+    class OrderTypes(models.TextChoices):
+        DELIVERY = ("D", "Delivery")
+        PICKUP = ("P", "Pickup")
+        
+        
+    class OrderStatus(models.TextChoices):
+        PENDING = ("P", "Pending")
+        APPROVED = ("A", "Approved")
+        REJECTED = ("R", "Rejected")
+    
+    
+    class Meta:
+        verbose_name_plural = "Orders"
+    
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    delivery = models.ForeignKey(CCDelivery, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(editable=False)
+    type = models.CharField(choices=OrderTypes.choices, default=OrderTypes.PICKUP, max_length=1)
+        
+        
+    def save(self, *args, **kwargs):
+        if not self.created_at:
+            self.created_at = timezone.now()
+        return super(Order, self).save(*args, **kwargs)
