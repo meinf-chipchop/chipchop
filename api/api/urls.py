1bb94e4d9c2eeec477e853e058576ff199525484
Marc Gaspà Joval <mgaspjoval2002@gmail.com>
2024-10-16 18:40:04 +0200

    arreglar errors

--- api/api/urls.py
+++ api/api/urls.py
@@ -16,37 +16,8 @@ router.register(r"cooks", cooks.viewsets.CCCookViewSet, basename="cook")
 router.register(r'users', users.viewsets.UserViewSet, basename="user")
 router.register(r'tokens', users.viewsets.TokenViewSet, basename="token")
 router.register(r'account-approvals', petitions.viewsets.AccountUpgradePetitionViewset, basename="account-approval")
+router.register(r"deliverers", deliverers.viewsets.CCDelivererViewSet, basename="deliverer")
 
-
-router.register(
-    r"cooks",
-    cooks.viewsets.CCCookViewSet,
-    basename="cook",
-)
-
-router.register(
-    r"deliverers",
-    deliverers.viewsets.CCDelivererViewSet,
-    basename="deliverer",
-)
-
-router.register(
-    r"users",
-    users.viewsets.UserViewSet,
-    basename="user",
-)
-
-router.register(
-    r"tokens",
-    users.viewsets.TokenViewSet,
-    basename="token",
-)
-
-router.register(
-    r"account-approvals",
-    petitions.viewsets.AccountUpgradePetitionViewset,
-    basename="account-approval",
-)
 
 
 urlpatterns = [
