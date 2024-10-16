89dd952b1f3e53a5315ffe7e65930b7bfa8f28c5
Marc Gaspà Joval <mgaspjoval2002@gmail.com>
2024-10-16 20:06:10 +0200

    cors

--- api/app/settings.py
+++ api/app/settings.py
@@ -49,11 +49,13 @@ INSTALLED_APPS = [
     "api",
     "cooks",
     "deliverers",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -62,6 +64,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "app.urls"
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
