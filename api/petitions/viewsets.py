dbe28beb40c380285f98a2c936b16806f709cd26
drownedd <drwn.it@gmail.com>
2024-10-21 17:03:58 +0100

    Added enpoint

--- api/petitions/viewsets.py
+++ api/petitions/viewsets.py
@@ -3,18 +3,24 @@ from rest_framework import viewsets, mixins
 from . import models, serializers
 
 
-class AccountUpgradePetitionViewset(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet):  
-  serializer_class = serializers.AccountUpgradePetitionSerializer
-  
-  def get_queryset(self):
-    user = self.request.user
-    if user.is_staff:  # Check if the user is an admin
-        return models.AccountUpgradePetition.objects.all()
-    else:
-        return models.AccountUpgradePetition.objects.filter(user=user)
+class AccountUpgradePetitionViewset(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
+    serializer_class = serializers.AccountUpgradePetitionSerializer
 
-  def get_serializer_class(self):
-    if self.action == 'list':
-      return serializers.AccountUpgradePetitionListSerializer
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:  # Check if the user is an admin
+            return models.AccountUpgradePetition.objects.all()
+        else:
+            return models.AccountUpgradePetition.objects.filter(user=user)
 
-    return super().get_serializer_class()
+    def get_serializer_class(self):
+        if self.action == "list":
+            return serializers.AccountUpgradePetitionListSerializer
+
+        return super().get_serializer_class()
