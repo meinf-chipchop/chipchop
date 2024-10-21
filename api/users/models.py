85567f0a6f2593e719a4bbccc9fb74ab4fbaf769
drownedd <drwn.it@gmail.com>
2024-10-21 14:39:01 +0100

    Refactored user cook/deliverer creation

--- api/users/models.py
+++ api/users/models.py
@@ -2,6 +2,30 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from django.contrib.auth.models import UserManager
+
+
+class CCUserManager(UserManager):
+
+    def create_user(self, **kwargs):
+
+        print("CUSTOM CREATE USER WITH KWARGS", kwargs)
+
+        from petitions.models import AccountUpgradePetition
+
+        role_in_kwargs = "role" in kwargs
+        if not role_in_kwargs:
+            kwargs["role"] = CCUser.UserRoles.USER
+
+        kwargs["username"] = "asd"
+
+        user = super().create_user(**kwargs)
+
+        if role_in_kwargs:
+            AccountUpgradePetition.objects.create(user=user).save()
+
+        return user
+
 
 class CCUser(AbstractUser):
 
@@ -20,6 +44,8 @@ class CCUser(AbstractUser):
     role = models.CharField(
         choices=UserRoles.choices, default=UserRoles.USER, max_length=1
     )
+
+    objects = CCUserManager()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["age", "password"]
