c78e5481487f9065bde7c39ebbe5125254e6f232
Marc Gaspà Joval <mgaspjoval2002@gmail.com>
2024-11-06 19:01:38 +0100

    added allowed url for expo

--- api/app/settings.py
+++ api/app/settings.py
@@ -85,17 +85,14 @@ ROOT_URLCONF = "app.urls"
 # CSRF / CORS / Cookies
 ALLOWED_HOSTS = ["*"]
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000",
     "http://localhost:3000",
+    "http://localhost:8081",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8081",
     "http://194.164.171.6",
     "https://chipchop.mooo.com",
 ]
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://194.164.171.6",
-    "https://chipchop.mooo.com",
-]
+CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS.copy()
 
 
 TEMPLATES = [
