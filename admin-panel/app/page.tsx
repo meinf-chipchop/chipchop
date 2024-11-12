11a91fd8dd0fb834ba0ce449b6b64ae2afd266b3
drownedd <drwn.it@gmail.com>
2024-11-12 19:32:38 +0000

    Sonarqube minor issues fixing

--- admin-panel/app/page.tsx
+++ admin-panel/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 import { LockIcon } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 
