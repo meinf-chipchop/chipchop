ee6f8d11bbbb8ea5e4799fbf72772db2d364c780
Marc Gaspà Joval <mgaspjoval2002@gmail.com>
2024-12-17 16:01:29 +0100

    fixed problems with me

--- android/app/(main)/_layout.tsx
+++ android/app/(main)/_layout.tsx
@@ -4,19 +4,6 @@ import { Colors } from "@/constants/Colors";
 import { useSession } from "@/auth/authContext";
 
 export default function Layout() {
-  const { session, isLoading } = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!isLoading) {
-      if (session) {
-        router.push("/(main)/(tabs)/home"); // Redirect to home if logged in
-      } else {
-        router.push("/(auth)/onboarding"); // Redirect to login if not logged in
-      }
-    }
-  }, [session, isLoading]);
-
   return (
     <Stack
       screenOptions={{
