b9d131d586a5fdbdff4bc4e08771edbc846d3783
Marc Gaspà Joval <mgaspjoval2002@gmail.com>
2024-12-17 15:30:30 +0100

    perfi crec que is

--- api/app/settings.py
+++ api/app/settings.py
@@ -38,6 +38,7 @@ if DEV_MODE:
     print("Running in development mode")
     CSRF_COOKIE_SAMESITE = 'None'
     SESSION_COOKIE_SAMESITE = 'None'
+    SESSION_COOKIE_SECURE = True
     CORS_ORIGIN_ALLOW_ALL = True
     CORS_ALLOW_CREDENTIALS = True
     CSRF_USE_SESSIONS = False
